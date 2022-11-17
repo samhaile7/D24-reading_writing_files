f4fc57ad7e9499c02d41c6a5cb85843593240478
Samuel Haile <samhaile129@gmail.com>
2022-11-16 23:09:35 -0500

    demo

--- main.py
+++ main.py
@@ -4,12 +4,12 @@
 
 # Another way of achieving the same as above
 
-# with open("my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
-
-with open("my_file.txt", mode='a') as file:
-    file.write("\nNew line written in python")
-
-with open("new_file.txt", mode="a") as file:
-    file.write("\nthis is the new file")
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+#
+# with open("my_file.txt", mode='a') as file:
+#     file.write("\nNew line written in python")
+#
+# with open("new_file.txt", mode="a") as file:
+#     file.write("\nthis is the new file")
