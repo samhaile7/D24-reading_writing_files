19fb350b92de000fca8d88b9b175e26a146c57a5
Samuel Haile <samhaile129@gmail.com>
2022-11-16 22:04:31 -0500

    new file

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-file = open("my_file.txt")
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+
+# Another way of achieving the same as above
+
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+with open("my_file.txt", mode='a') as file:
+    file.write("\nNew line written in python")
+
+with open("new_file.txt", mode="a") as file:
+    file.write("\nthis is the new file")
